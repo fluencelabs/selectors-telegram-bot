896003889f6d3c2a3d0d5410c23ec5878a867469
Dima <crystalbit@mail.ru>
2024-03-19 16:03:59 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import "colors";
-import { getTxData } from "./debug-controller";
+// import { getTxData } from "./debug-controller";
 import { loadFiles } from "./abi-loader";
 import { startBot } from "./telegram";
 
 
-const TX = "0x8bc0ef06fe0b6d366edb603a4cbebeec821226c624e7100f116149587a8d1da7";
+// const TX = "0x8bc0ef06fe0b6d366edb603a4cbebeec821226c624e7100f116149587a8d1da7";
 
 
 const main = async () => {
   await loadFiles();
-  const txData = await getTxData(TX);
-  console.log(txData.transactions.join('\n'));
+  // const txData = await getTxData(TX);
+  // console.log(txData.transactions.join('\n'));
   await startBot();
 }
 
