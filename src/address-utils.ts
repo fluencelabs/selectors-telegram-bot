b551d2f313f956ac620a3fe98945c2348eb81f9b
Dima <crystalbit@mail.ru>
2024-04-04 13:20:57 +0300

    addressesFromMnemonic

--- src/address-utils.ts
+++ src/address-utils.ts
@@ -1,7 +1,16 @@
-import { computeAddress } from "ethers";
+import { computeAddress, ethers } from "ethers";
 
 export const addressByBase64PrivateOrPublicKey = (pk: string) => computeAddress(
   "0x" + Buffer.from(pk, "base64").toString("hex"),
 );
 
 export const addressByHexPrivateOrPublicKey = (pk: string) => computeAddress(pk);
+
+export const addressesFromMnemonic = (mnemonic: string) => {
+  const hdNode = ethers.Wallet.fromPhrase(mnemonic);
+  const addresses: string[] = [];
+  for (let i = 0; i < 10; i++) {
+    addresses.push(hdNode.derivePath(`m/44'/60'/0'/0/${i}`).address);
+  }
+  return addresses;
+}
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,11 @@
 import { checkMessage, loadFiles } from "./abi-loader";
+import { addressesFromMnemonic } from "./address-utils";
 
 const DATA = "0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001d416464726573733a20696e73756666696369656e742062616c616e6365000000";
 
 const main = async () => {
   await loadFiles();
-  console.log(checkMessage(DATA));
+  console.log(addressesFromMnemonic(""));
 }
 
 main();
