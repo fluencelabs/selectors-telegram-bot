7108114436d31ee8ac0c5b0df0c05eaea96cf0f3
Dima <crystalbit@mail.ru>
2024-04-30 10:32:03 +0200

    npx fix - debug

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 import { processMessage } from './abi-service';
+import { intro } from './intro';
 
 const commandArgs = process.argv.slice(2);
 const message = commandArgs.join(' ');
 
+if (message.trim() === '') {
+  console.log(intro);
+  process.exit(0);
+}
+
+console.log('Processing message... |', message, '|');
 console.log(processMessage(message));
